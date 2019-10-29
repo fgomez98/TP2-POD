2907a80fe626817c54804b90b1ccc7848642951b
pedroremigiopingarilho <premigio@itba.edu.ar>
2019-10-29 20:04:35 -0300

    args4j

---
+++ client/src/main/java/tp2/client/CmdParserUtils.java
@@ -0,0 +1,23 @@
+package tp2.client;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import java.io.IOException;
+
+class CmdParserUtils {
+
+    static void init(final String[] args, Object o) throws IOException {
+        final CmdLineParser parser = new CmdLineParser(o);
+        if (args.length < 1) {
+            parser.printUsage(System.err);
+            System.exit(1);
+        }
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.out.println(e.getMessage());
+            parser.printUsage(System.err);
+            System.exit(1);
+        }
+    }
+}
