b9d850b611b0e7b204d6a8885647c9159f65d002
Emilio Basualdo <Pilobas97@gmail.com>
2019-10-31 17:08:16 -0300

    query6 done

---
+++ api/src/main/java/query6/Query6Mapper.java
@@ -0,0 +1,18 @@
+package query6;
+
+import com.hazelcast.mapreduce.Context;
+import com.hazelcast.mapreduce.Mapper;
+import tpe2.api.Flight;
+
+public class Query6Mapper implements Mapper<String, Flight, String, Long> {
+    private static final Long ONE = 1L;
+    @Override
+    public void map(String key, Flight flight, Context<String, Long> context) {
+        int comp = flight.getOaciOrigin().compareTo(flight.getOaciDestination());
+        if (comp < 0) {
+            context.emit(flight.getOaciOrigin()+";"+flight.getOaciDestination(), ONE);
+        } else if (comp > 0) {
+            context.emit(flight.getOaciDestination()+";"+flight.getOaciOrigin(), ONE);
+        } // else { // comp == 0 considerado
+    }
+}
