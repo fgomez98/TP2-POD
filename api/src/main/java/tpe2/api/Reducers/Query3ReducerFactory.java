1180b3237e80effbe4c2c5aefe6a126a956f154c
Fermin <fgomez@itba.edu.ar>
2019-11-03 03:07:07 -0300

    Orden de la permutacion de tuplas en query 3

--- api/src/main/java/tpe2/api/Reducers/Query3ReducerFactory.java
+++ api/src/main/java/tpe2/api/Reducers/Query3ReducerFactory.java
@@ -35,21 +35,13 @@ public class Query3ReducerFactory implements ReducerFactory<Long, String, List<T
         @Override
         public List<Tuple<String, String>> finalizeReduce() {
             // lista con pares de oaci sin repetir combinaciones
+            oacis.sort(String::compareTo);
             List<Tuple<String, String>> list = new ArrayList<>();
             for (int i = 0; i < oacis.size(); i++) {
                 for (int j = i + 1; j < oacis.size(); j++) {
                     list.add(new Tuple<>(oacis.get(i), oacis.get(j)));
                 }
             }
-            // ordenamos alfabeticamente los pares de oaci
-            list.sort((t1, t2) -> {
-                        if (t1.getaVal().compareTo(t2.getaVal()) == 0) { // primer oaci es igual entonces comparamos con el segundo
-                            return t1.getbVal().compareTo(t2.getbVal());
-                        } else {
-                            return t1.getaVal().compareTo(t2.getaVal());
-                        }
-                    }
-            );
             return list;
         }
     }
